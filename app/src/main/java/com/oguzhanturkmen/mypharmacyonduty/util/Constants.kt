e611ee62797b65b67fe7de9e48de0d2f0ad283ac
Oğuzhan Türkmen <oguzhn.1132@gmail.com>
2022-12-11 14:02:11 +0300

    api

--- app/src/main/java/com/oguzhanturkmen/mypharmacyonduty/util/Constants.kt
+++ app/src/main/java/com/oguzhanturkmen/mypharmacyonduty/util/Constants.kt
@@ -3,7 +3,7 @@ package com.oguzhanturkmen.mypharmacyonduty.util
 object Constants {
 //https://www.nosyapi.com/apiv2/pharmacy
     const val BASE_URL = "https://www.nosyapi.com/apiv2/"
-    const val API_KEY = "Bearer fN8SW5SOba3sRMfsm7wZuaBOmbuiux70LjuIDOdQSEdL3ZwrIQC4sKlguVm2"
+    const val API_KEY = "Bearer Fsr1Cubo57kdUNFR9RTA1RaHSN0jxcgxWob0Pw3c85w2R5K2n1bTuYpZT3oE"
     const val CONTENT = "application/json"
 
 }
